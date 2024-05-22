da92a78017c18c3f70da0f7f9f7b9fc6995456fe
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-05-22 01:25:42 +0100

    responsivness

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import "../style/components/header.css";
 import "../style/components/hero.css";
 import "../style/components/team.css";
 import "../style/components/sign-in.css";
+import "../style/components/contact.css";
 import "../style/utility.css";
